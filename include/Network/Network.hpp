bd712b28b0246ef903491b679957a66a124bb75b
Majed Shakir <majedshakir@potentialmotors.com>
2022-01-07 17:32:24 -0400

    Add device to the lstm hidden layers

--- include/Network/Network.hpp
+++ include/Network/Network.hpp
@@ -3,14 +3,12 @@
 #include <torch/torch.h>
 
 struct Net : torch::nn::Module {
-  Net():  fc1(torch::nn::LinearOptions(20, 15).bias(true)), 
-          fc2(torch::nn::LinearOptions(15, 10).bias(true)), 
-          middel(torch::nn::LSTMOptions(5, 5).num_layers(5).batch_first(true).bidirectional(true)),
-          fc3(torch::nn::LinearOptions(10, 10).bias(true)), 
-          fc4(torch::nn::LinearOptions(10, 20).bias(true)){
-    //--------------------------
-    std::get<0>(_gates) = h0;
-    std::get<1>(_gates) = c0;
+  Net(torch::Device& device):   m_device(device),
+                                fc1(torch::nn::LinearOptions(20, 15).bias(true)), 
+                                fc2(torch::nn::LinearOptions(15, 10).bias(true)), 
+                                middel(torch::nn::LSTMOptions(5, 5).num_layers(64).batch_first(true).bidirectional(true)),
+                                fc3(torch::nn::LinearOptions(10, 10).bias(true)), 
+                                fc4(torch::nn::LinearOptions(10, 20).bias(true)){
     //--------------------------
     register_module("fc1", fc1);
     register_module("fc2", fc2);
@@ -18,7 +16,7 @@ struct Net : torch::nn::Module {
     register_module("fc3", fc3);
     register_module("fc4", fc4);
     //--------------------------
-  }// end Net()
+  }// end Net(torch::Device& device)
   //--------------------------------------------------------------
   torch::Tensor forward(torch::Tensor x){
     //--------------------------
@@ -26,10 +24,10 @@ struct Net : torch::nn::Module {
     //--------------------------
     x = torch::relu(fc2->forward(x));
     //--------------------------
-    std::get<0>(_gates) = h0;
-    std::get<1>(_gates) = c0;
+    std::get<0>(_gates) = h0.to(m_device);
+    std::get<1>(_gates) = c0.to(m_device);
     //--------------------------
-    auto _input_lstm = x.view({-1, 2, 5});
+    auto _input_lstm = x.view({-1, 2, 5}).to(m_device);
     auto x_lstm = middel->forward(_input_lstm, _gates);
     //--------------------------
     h0 = std::get<0>(std::get<1>(x_lstm));
@@ -45,14 +43,16 @@ struct Net : torch::nn::Module {
   //--------------------------------------------------------------
   private:
     //--------------------------------------------------------------
+    torch::Device m_device;
+    //--------------------------
     torch::nn::Linear fc1;
     torch::nn::Linear fc2;
     torch::nn::LSTM middel;
     torch::nn::Linear fc3;
     torch::nn::Linear fc4;
     //--------------------------
-    torch::Tensor h0 = torch::from_blob(std::vector<float>(1*5*2, 0.0).data(), {10, 1, 5});
-    torch::Tensor c0 = torch::from_blob(std::vector<float>(1*5*2, 0.0).data(), {10, 1, 5});
+    torch::Tensor h0 = torch::from_blob(std::vector<float>(1*64*2, 0.0).data(), {128, 1, 5});
+    torch::Tensor c0 = torch::from_blob(std::vector<float>(1*64*2, 0.0).data(), {128, 1, 5});
     //--------------------------
     std::tuple<torch::Tensor, torch::Tensor> _gates;
     std::tuple<torch::Tensor, std::tuple<torch::Tensor, torch::Tensor>> _input;

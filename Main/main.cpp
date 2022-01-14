12f7012d3d603764177b7d7582c3d3ecfecb46ad
Majed Shakir <majedshakir@potentialmotors.com>
2022-01-14 12:07:02 -0400

    Change: increase the data generated

--- Main/main.cpp
+++ Main/main.cpp
@@ -38,7 +38,7 @@ int main(){
     //--------------------------
     for (size_t i = 0; i < 5; i++){
         //--------------------------
-        Generate _generate(random_radius(rng), 600); 
+        Generate _generate(random_radius(rng), 60000); 
         GenerateDate data = _generate.get_data();
         GenerateDate test_data = _generate.get_test();
         //------------

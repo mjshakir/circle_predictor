fc1aa00d3420f54a1acd27e831b8b8d67d883ae6
Majed Shakir <majedshakir@potentialmotors.com>
2022-01-26 00:47:40 -0400

    Add: circle center

--- include/Generate/Generate.hpp
+++ include/Generate/Generate.hpp
@@ -8,9 +8,9 @@ class Generate{
         //--------------------------------------------------------------
         Generate() = delete;
         //--------------------------
-        Generate(const double& radius = 1, const size_t& generated_points = 60000);
+        Generate(const double& radius = 1, const size_t& generated_points = 60000, const std::tuple<double, double>& center = {0,0});
         //--------------------------
-        Generate(const torch::Tensor& x_value, const double& radius = 1, const size_t& generated_points = 60000);
+        Generate(const torch::Tensor& x_value, const double& radius = 1, const size_t& generated_points = 60000, const std::tuple<double, double>& center = {0,0});
         //--------------------------
         torch::Tensor get_input(void); 
         //--------------------------
@@ -19,6 +19,8 @@ class Generate{
         std::tuple<torch::Tensor, torch::Tensor> get_data(void);
         //--------------------------
         std::tuple<torch::Tensor, torch::Tensor> get_validation(void);
+        //--------------------------
+        std::tuple<double, double> get_center(void);
         //--------------------------
         double get_radius(void);
         //--------------------------------------------------------------
@@ -32,6 +34,8 @@ class Generate{
         const std::tuple<torch::Tensor, torch::Tensor> generate_validation_value(const double& radius);
         //--------------------------
         const torch::Tensor generate_value(const torch::Tensor& x_value, const double& radius);
+        //--------------------------
+        const std::vector<double> generate_value(const std::vector<double>& x_value, const double& radius);
         //--------------------------------------------------------------
     private:
         //--------------------------
@@ -42,6 +46,8 @@ class Generate{
         size_t m_generated_points;
         //--------------------------
         torch::Tensor m_x_value, y_value;
+        //--------------------------
+        std::tuple<double, double> m_center;
         //--------------------------
         std::tuple<torch::Tensor, torch::Tensor> full_data, validation_data;
         //--------------------------------------------------------------

ac4354b1731ef9ed1fcebd0b97f2cc1c90e94fa0
Majed Shakir <majedshakir@potentialmotors.com>
2022-01-31 17:49:07 -0400

    Add: more hidden layer

--- include/Network/Network.hpp
+++ include/Network/Network.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <torch/torch.h>
-#include <future>
 
 struct Net : torch::nn::Module {
   //--------------------------------------------------------------
@@ -10,9 +9,9 @@ struct Net : torch::nn::Module {
     Net(torch::Device& device):   m_device(device),
                                   input_layer(torch::nn::LinearOptions(20, 128).bias(true)), 
                                   features(torch::nn::LinearOptions(128, 512).bias(true)), 
-                                  features2(torch::nn::LinearOptions(532, 1024).bias(true)), // 640
-                                  output_layer(torch::nn::LinearOptions(1536, 40).bias(true)),
-                                  recurrent_layer(torch::nn::LSTMOptions(10, 128).num_layers(64).batch_first(true).bidirectional(true)){
+                                  features2(torch::nn::LinearOptions(532, 544).bias(true)), //1024
+                                  output_layer(torch::nn::LinearOptions(1024, 40).bias(true)), //1536
+                                  recurrent_layer(torch::nn::LSTMOptions(10, 80).num_layers(64).batch_first(true).bidirectional(true)){ // 128
       //--------------------------
       register_module("input_layer", input_layer);
       register_module("features", features);
@@ -24,12 +23,19 @@ struct Net : torch::nn::Module {
     //--------------------------------------------------------------
     torch::Tensor forward(torch::Tensor& x){
       //--------------------------
+      // auto x_linear = std::async(std::launch::async, [&](){return linear_layers(x);});
       auto x_linear = linear_layers(x);
-      auto x_lstm =  lstm_layers(x);
+      auto x_lstm = lstm_layers(x);
       //--------------------------
       auto out_results = torch::cat({x_linear, x_lstm});
       //--------------------------
+      torch::dropout(out_results, /*p=*/0.5, /*training=*/is_training());
+      //--------------------------
       return output_layer->forward(out_results);
+      //--------------------------
+      // torch::dropout(x_linear, /*p=*/0.5, /*training=*/is_training());
+      //-------------------------
+      // return output_layer->forward(x_linear);
       //--------------------------
     }// end torch::Tensor forward(torch::Tensor x)
     //--------------------------------------------------------------
@@ -45,11 +51,7 @@ struct Net : torch::nn::Module {
       //--------------------------
       auto cat_results = torch::cat({_results, x});
       //--------------------------
-      _results = torch::relu(features2->forward(cat_results));
-      //-------------------------
-      torch::dropout(_results, /*p=*/0.5, /*training=*/is_training());
-      //-------------------------
-      return _results;
+      return torch::relu(features2->forward(cat_results));
       //-------------------------
     }// end torch::Tensor linear_layers(torch::Tensor& x)
     //--------------------------------------------------------------
@@ -58,7 +60,7 @@ struct Net : torch::nn::Module {
       std::get<0>(_gates) = h0.to(m_device);
       std::get<1>(_gates) = c0.to(m_device);
       //--------------------------
-      auto _input_lstm = x.view({-1, 2, 10}).to(m_device);
+      auto _input_lstm = x.view({-1, 2, 80}).to(m_device);
       //--------------------------
       auto x_lstm = recurrent_layer->forward(_input_lstm, _gates);
       //--------------------------
@@ -79,8 +81,8 @@ struct Net : torch::nn::Module {
     //--------------------------
     torch::nn::LSTM recurrent_layer;
     //--------------------------
-    torch::Tensor h0 = torch::from_blob(std::vector<float>(1*64*2, 0.0).data(), {64*2, 1, 128});
-    torch::Tensor c0 = torch::from_blob(std::vector<float>(1*64*2, 0.0).data(), {64*2, 1, 128});
+    torch::Tensor h0 = torch::from_blob(std::vector<float>(1*64*2, 0.0).data(), {64*2, 1, 80});
+    torch::Tensor c0 = torch::from_blob(std::vector<float>(1*64*2, 0.0).data(), {64*2, 1, 80});
     //--------------------------
     std::tuple<torch::Tensor, torch::Tensor> _gates;
     std::tuple<torch::Tensor, std::tuple<torch::Tensor, torch::Tensor>> _input;

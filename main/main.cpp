28b828880d05b53d29edde55e43355bb8c718428
Majed Shakir <majedshakir@potentialmotors.com>
2022-02-13 06:24:11 -0400

    Add: train LSTM network

--- main/main.cpp
+++ main/main.cpp
@@ -10,10 +10,12 @@ int main(int argc, char const *argv[]){
     if (argc >= 4){
         throw std::invalid_argument("More arugments then can be allocation");
     }// end if (argc >= 4)
+    //--------------------------------------------------------------
+    // Command line arugments for training size and data generation 
     //--------------------------
-    size_t training_size = 100, generated_size = 10000;
+    size_t training_size{100}, generated_size{10000};
     //--------------------------
-    if (argc < 3 and argc > 1){
+     if (argc > 1){
         //--------------------------
         if (std::atoi(argv[1]) > 0){
             //--------------------------
@@ -21,35 +23,33 @@ int main(int argc, char const *argv[]){
             //--------------------------
         }// end if (std::atoi(argv[1]) > 0) 
         //--------------------------
-    }// end if (argc < 3 and argc > 1)
+    }// end if (argc > 1)
     //-----------
-    if (argc < 4 and argc > 2){
+    if (argc > 2){
         //--------------------------
-        if (std::atoi(argv[1]) > 0){
-            //--------------------------
-            training_size = std::stoul(argv[1]);
-            //--------------------------
-        }// end if (std::atoi(argv[1]) > 0) 
-        //--------------------------
-        if (std::atoi(argv[2]) > 40){
+        if (std::atoi(argv[2]) > 200){
             //--------------------------
             generated_size = std::stoul(argv[2]);
             //--------------------------
         }// end if (std::atoi(argv[1]) > 0)
         else{
             //--------------------------
-            throw std::out_of_range("Must be at least 40 (x > 40)");
+            throw std::out_of_range("Must be at least 200 (x > 200)");
             //--------------------------
         }// end else
         //-------------------------- 
-    }// end if (argc < 3 and argc > 2)
+    }// end if (argc > 2)
     //--------------------------
     std::cout << " training_size: " << training_size << " generated_size: " << generated_size << std::endl;
+    //--------------------------------------------------------------
+    // Creating a random number generator
     //--------------------------
     std::random_device dev;
     std::mt19937 rng(dev()), center_rng(dev());
     std::uniform_real_distribution<double> random_radius(1,10);
     std::uniform_int_distribution<> random_centers(-10,10);
+    //--------------------------------------------------------------
+    // Initiate Torch seed, device type
     //--------------------------
     torch::manual_seed(17);
     //--------------------------
@@ -67,12 +67,14 @@ int main(int argc, char const *argv[]){
     //--------------------------
     torch::Device device(device_type);
     //--------------------------
-    Net model;
+    // Net model;
+    // model.to(device);
+    LSTMNet model(device);
     model.to(device);
     //--------------------------
     torch::optim::SGD optimizer(model.parameters(), torch::optim::SGDOptions(1E-1L).momentum(0.95).nesterov(true));
     //--------------------------
-    NetworkHandling<Net> handler(model, device);
+    NetworkHandling<LSTMNet> handler(model, device);
     //--------------------------
     for (size_t i = 0; i < training_size; i++){
         //--------------------------
@@ -87,26 +89,31 @@ int main(int argc, char const *argv[]){
         // Generate your data set. At this point you can add transforms to you data set, e.g. stack your
         // batches into a single tensor.
         // auto data_set = DataLoader(std::move(std::get<0>(data)), std::move(std::get<1>(data))).map(torch::data::transforms::Normalize<>(0.5, 0.25)).map(torch::data::transforms::Stack<>());
+        //--------------------------
         auto data_set = DataLoader( Normalize::normalization(std::get<0>(data)), 
                                     Normalize::normalization(std::get<1>(data))).map(torch::data::transforms::Stack<>());
         //--------------------------
         // Generate a data loader.
+        //--------------------------
         auto data_loader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>( std::move(data_set), 
                                                                                                 torch::data::DataLoaderOptions(20));
         //--------------------------
         // Generate your data set. At this point you can add transforms to you data set, e.g. stack your
         // batches into a single tensor.
+        //--------------------------
         auto validation_data_set = DataLoader(  Normalize::normalization(std::get<0>(validation_data)), 
                                                 Normalize::normalization(std::get<1>(validation_data)))
                                                     .map(torch::data::transforms::Stack<>());
         //--------------------------
         // Generate a data loader.
+        //--------------------------
         auto validation_data_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(std::move(validation_data_set), 
                                                                                                         torch::data::DataLoaderOptions(20));
 
         //--------------------------
         Timing _timer(__FUNCTION__);
-        auto loss = handler.train(std::move(data_loader), std::move(validation_data_loader), optimizer, 2.5E-1L);
+        // auto loss = handler.train(std::move(data_loader), std::move(validation_data_loader), optimizer, 2.5E-1L);
+        auto loss = handler.train(std::move(data_loader), optimizer, 100);
         //--------------------------
         printf("\n-----------------Done:[%zu]-----------------\n", i);
         //--------------------------

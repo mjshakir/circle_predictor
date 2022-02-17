175919ca265e85d996e85bf8082adb4856574021
Majed Shakir <majedshakir@potentialmotors.com>
2022-02-17 13:14:19 -0400

    Add: utility library

--- include/Network/Network.hpp
+++ include/Network/Network.hpp
@@ -1,52 +1,6 @@
 #pragma once
 
-#include <torch/torch.h>
-//--------------------------------------------------------------
-struct Net : torch::nn::Module {
-  //--------------------------------------------------------------
-  public:
-    //--------------------------
-    Net(); 
-    //--------------------------
-    torch::Tensor forward(torch::Tensor& x);
-    //--------------------------------------------------------------
-  protected:
-    //--------------------------------------------------------------
-    torch::Tensor linear_layers(const torch::Tensor& x);
-    //--------------------------------------------------------------
-  private:
-    //--------------------------------------------------------------
-    torch::nn::Linear input_layer;
-    torch::nn::Linear features;
-    torch::nn::Linear features2;
-    torch::nn::Linear output_layer;
-    //--------------------------------------------------------------
-}; // end struct Net : torch::nn::Module
-//--------------------------------------------------------------
-struct LSTMNet : torch::nn::Module {
-  //--------------------------------------------------------------
-  public:
-    //--------------------------
-    LSTMNet(const torch::Device& device);
-    //--------------------------------------------------------------
-    torch::Tensor forward(const torch::Tensor& x);
-    //--------------------------------------------------------------
-  protected:
-    //--------------------------------------------------------------
-    torch::Tensor lstm_layers(const torch::Tensor& x);
-    //--------------------------------------------------------------
-  private:
-    //--------------------------------------------------------------
-    torch::Device m_device;
-    //--------------------------
-    torch::nn::LSTM recurrent_layer;
-    //--------------------------
-    torch::Tensor h0 = torch::from_blob(std::vector<float>(1*20*2, 0.0).data(), {20*2, 1, 10});
-    torch::Tensor c0 = torch::from_blob(std::vector<float>(1*20*2, 0.0).data(), {20*2, 1, 10});
-    //--------------------------
-    std::tuple<torch::Tensor, torch::Tensor> _gates;
-    std::tuple<torch::Tensor, std::tuple<torch::Tensor, torch::Tensor>> _input;
-    //--------------------------------------------------------------
-}; // end struct LSTMNet : torch::nn::Module
-//--------------------------------------------------------------
-
+#include "Network/Networks.hpp"
+#include "Network/NetworkHandling.hpp"
+#include "Network/DataLoader.hpp"
+#include "Network/Normalize.hpp"

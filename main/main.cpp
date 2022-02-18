9413a3186b12244379e01d365ada9c82f6c2790b
Majed Shakir <majedshakir@potentialmotors.com>
2022-02-17 22:42:23 -0400

    Add: comments

--- main/main.cpp
+++ main/main.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <fstream>
 
+//--------------------------------------------------------------
 int main(int argc, char const *argv[]){
     //--------------------------
     if (argc >= 6){
@@ -65,7 +66,10 @@ int main(int argc, char const *argv[]){
         //-------------------------- 
     }// end if (argc > 3)
     //--------------------------
-    // std::cout << "training_size: " << training_size << " generated_size: " << generated_size << " epoch: " << epoch << std::endl;
+    std::cout << "training_size: " << training_size 
+    << "\ngenerated_size: " << generated_size 
+    << "\nprecision: " << precision
+    << "\nepoch: " << epoch << std::endl;
     //--------------------------------------------------------------
     // Creating a random number generator
     //--------------------------
@@ -91,6 +95,8 @@ int main(int argc, char const *argv[]){
     }// end else
     //--------------------------
     torch::Device device(device_type);
+    //--------------------------------------------------------------
+    // Initialize the network
     //--------------------------
     Net model;
     model.to(device);
@@ -100,34 +106,36 @@ int main(int argc, char const *argv[]){
     torch::optim::SGD optimizer(model.parameters(), torch::optim::SGDOptions(1E-1L).momentum(0.95).nesterov(true));
     //--------------------------
     NetworkHandling<Net> handler(model, device);
+    //--------------------------------------------------------------
+    // Training loop
     //--------------------------
     for (size_t i = 0; i < training_size; i++){
+        //--------------------------------------------------------------
+        // Generate data
         //--------------------------
         Generate _generate(random_radius(rng), generated_size, {random_centers(center_rng), random_centers(center_rng)}); 
-        auto data = _generate.get_data();
-        auto validation_data = _generate.get_validation();
         //------------
         std::cout   << "Training data radius: " << _generate.get_radius() 
                     << " at center: (" << std::get<0>(_generate.get_center()) << "," 
                     << std::get<1>(_generate.get_center()) << ")" << std::endl;
-        //--------------------------
+        //--------------------------------------------------------------
         // Generate your data set. At this point you can add transforms to you data set, e.g. stack your
         // batches into a single tensor.
         // auto data_set = DataLoader(std::move(std::get<0>(data)), std::move(std::get<1>(data))).map(torch::data::transforms::Normalize<>(0.5, 0.25)).map(torch::data::transforms::Stack<>());
         //--------------------------
-        auto data_set = DataLoader( Normalize::normalization(std::get<0>(data)), 
-                                    Normalize::normalization(std::get<1>(data))).map(torch::data::transforms::Stack<>());
+        auto data_set = DataLoader( Normalize::normalization(std::get<0>(_generate.get_data())), 
+                                    Normalize::normalization(std::get<1>(_generate.get_data()))).map(torch::data::transforms::Stack<>());
         //--------------------------
         // Generate a data loader.
         //--------------------------
         auto data_loader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>( std::move(data_set), 
                                                                                                 torch::data::DataLoaderOptions(20));
-        //--------------------------
-        // Generate your data set. At this point you can add transforms to you data set, e.g. stack your
+        //--------------------------------------------------------------
+        // Generate your validation data set. At this point you can add transforms to you data set, e.g. stack your
         // batches into a single tensor.
         //--------------------------
-        auto validation_data_set = DataLoader(  Normalize::normalization(std::get<0>(validation_data)), 
-                                                Normalize::normalization(std::get<1>(validation_data)))
+        auto validation_data_set = DataLoader(  Normalize::normalization(std::get<0>(_generate.get_validation())), 
+                                                Normalize::normalization(std::get<1>(_generate.get_validation())))
                                                     .map(torch::data::transforms::Stack<>());
         //--------------------------
         // Generate a data loader.
@@ -135,25 +143,32 @@ int main(int argc, char const *argv[]){
         auto validation_data_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(std::move(validation_data_set), 
                                                                                                         torch::data::DataLoaderOptions(20));
 
+        //--------------------------------------------------------------
+        // Time the loop
         //--------------------------
         Timing _timer(__FUNCTION__);
+        //--------------------------------------------------------------
+        // Train the network
+        //--------------------------
         auto loss = handler.train(std::move(data_loader), std::move(validation_data_loader), optimizer, precision);
         // auto loss = handler.train(std::move(data_loader), optimizer, epoch);
         //--------------------------
         printf("\n-----------------Done:[%zu]-----------------\n", i);
         //--------------------------
     }// end (size_t i = 0; i < 3; i++)
+    //--------------------------------------------------------------
+    // Generate test data
     //--------------------------
     Generate test_generate(random_radius(rng), 60, {random_centers(center_rng), random_centers(center_rng)});
     // Generate test_generate(5, 60, {0, 0}); 
-    auto test_data = test_generate.get_data();
+    // auto test_data = test_generate.get_data();
     //--------------------------
     std::cout   << "test data radius: " << test_generate.get_radius() 
                 << " at center: (" << std::get<0>(test_generate.get_center()) << "," 
                 << std::get<1>(test_generate.get_center()) << ")" << std::endl;
     //--------------------------
-    Normalize test_input_normal(std::get<0>(test_data));
-    Normalize test_target_normal(std::get<1>(test_data));
+    Normalize test_input_normal(std::get<0>(test_generate.get_data()));
+    Normalize test_target_normal(std::get<1>(test_generate.get_data()));
     //--------------------------
     // Generate your data set. At this point you can add transforms to you data set, e.g. stack your
     // batches into a single tensor.

b055bd93720ba518b625b2b2f9d89cb2c0dda5fc
Majed Shakir <majedshakir@potentialmotors.com>
2022-04-09 15:11:30 -0300

    Add: globle variables

--- include/Network/ReinforcementNetworkHandling.hpp
+++ include/Network/ReinforcementNetworkHandling.hpp
@@ -13,18 +13,22 @@ class ReinforcementNetworkHandling{
         //--------------------------------------------------------------
         ReinforcementNetworkHandling() = delete;
         //--------------------------
-        ReinforcementNetworkHandling(Network& model, const torch::Device& device){
+        ReinforcementNetworkHandling(Network& model, const torch::Device& device): m_model(model), m_device(device){
             //--------------------------
         }// end ReinforcementNetworkHandling(Network& model, torch::Device& device)
         //--------------------------
-        ReinforcementNetworkHandling(Network&& model, const torch::Device& device){
+        ReinforcementNetworkHandling(Network&& model, const torch::Device& device) : m_model(std::move(model)), m_device(device){
             //--------------------------
         }// end ReinforcementNetworkHandling(Network& model, torch::Device& device)
         //--------------------------
     //--------------------------------------------------------------
     protected:
         //--------------------------
-
+    private:
+        //--------------------------
+        Network m_model; 
+        torch::Device m_device;
+        //--------------------------
     //--------------------------------------------------------------
 };// end class ReinforcementNetworkHandling : protected NetworkHandling
 //--------------------------------------------------------------

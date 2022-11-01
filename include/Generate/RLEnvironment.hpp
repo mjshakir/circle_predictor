7b8278c9e2f3eba54179f3d5c4e97a455ef1252c
mjshakir <maslove23@gmail.com>
2022-11-01 13:31:03 -0300

    Change: make the function generaic by templates

--- include/Generate/RLEnvironment.hpp
+++ include/Generate/RLEnvironment.hpp
@@ -1,38 +1,36 @@
 #pragma once
-
+//--------------------------------------------------------------
+/* From: https://stackoverflow.com/questions/14803112/short-way-to-stdbind-member-function-to-object-instance-without-binding-param 
+    and https://stackoverflow.com/questions/70355767/binding-a-class-method-to-a-method-of-another-class */
 //--------------------------------------------------------------
 // LibTorch library
 //--------------------------------------------------------------
 #include <torch/torch.h>
 //--------------------------------------------------------------
-// template<typename INPUT, typename COST_OUTPUT, typename ...Args>
-template<typename COST_OUTPUT, typename C, typename... Args>
+// template<typename COST_OUTPUT, typename C, typename... Args>
+template<typename Dataset, typename COST_OUTPUT, typename... Args>
+
 class RLEnvironment{
     //--------------------------------------------------------------
     public:
         //--------------------------------------------------------------
         RLEnvironment(void) = delete;
         //--------------------------
-        template<typename Dataset>
-        RLEnvironment(Dataset&& data_loader){
-            //--------------------------
-            for (const auto& batch : *data_loader){
-                //--------------------------
-                auto input_data = batch.data, target_data = batch.target;
-                //--------------------------
-                _full_data.push_back({input_data, target_data});
-                //--------------------------
-            } //for (const auto& batch : *data_loader)
-            //--------------------------
-            m_iter = _full_data.begin();
-            m_iter_end = _full_data.end();
-            //--------------------------
+        RLEnvironment(Dataset&& data_loader) : m_data_loader(std::move{data_loader}){
+            //----------------------------
         }// end RLEnvironment(Dataset&& data_loader)
         //--------------------------
-        template<typename Functions>
-        void set_reward_function(Functions& function){
+        // template<typename Functions>
+        // void set_reward_function(Functions& function){
+        //     //--------------------------
+        //     m_CostFunction = object_bind(&function, this);
+        //     //--------------------------
+        // }// end void set_reward_function(Functions&& function);
+        //--------------------------
+        template<typename FUNCTION>
+        void set_reward_function(COST_OUTPUT (FUNCTION::*fun) (), FUNCTION *t){
             //--------------------------
-            m_CostFunction = object_bind(&function, this);
+            m_CostFunction = [t, fun](Args... args){ return (t->*fun) (args...); };
             //--------------------------
         }// end void set_reward_function(Functions&& function);
         //--------------------------------------------------------------
@@ -50,23 +48,23 @@ class RLEnvironment{
         //     return [=](Args... args){ return (instance.*func)(args...); };
         // }// end std::function<COST_OUTPUT(Args...)> objectBind(R (C::* func)(Args...), C& instance)
         //--------------------------
-        std::function<COST_OUTPUT(Args...)> objectBind(COST_OUTPUT (C::* func)(Args...), C& instance) {
-            return [=](Args... args){ return (instance.*func)(args...); };
-        }// end std::function<COST_OUTPUT(Args...)> objectBind(COST_OUTPUT (C::* func)(Args...), C& instance)
+        // std::function<COST_OUTPUT(Args...)> objectBind(COST_OUTPUT (C::* func)(Args...), C& instance) {
+        //     return [=](Args... args){ return (instance.*func)(args...); };
+        // }// end std::function<COST_OUTPUT(Args...)> objectBind(COST_OUTPUT (C::* func)(Args...), C& instance)
         //--------------------------
         std::tuple<torch::Tensor, COST_OUTPUT, bool> internal_step(Args... args){
             //--------------------------
-            auto [_input, _target] = *m_iter;
+            auto input = m_data_loader->data;
             //--------------------------
             auto _reward = m_CostFunction(args...);
             //--------------------------
-            if(m_iter == m_iter_end){
+            if(m_data_loader == m_data_loader->end()){
                 //--------------------------
                 return {_input, _reward, true};
                 //--------------------------
             }// if(m_iter.end())
             //--------------------------
-            ++m_iter;
+            ++m_data_loader;
             //--------------------------
             return {_input, _reward, false};
             //--------------------------
@@ -76,10 +74,7 @@ class RLEnvironment{
         //--------------------------------------------------------------
         Dataset m_data_loader;
         //--------------------------
-        std::vector<std::tuple<torch::Tensor, torch::Tensor>>::iterator m_iter, m_iter_end;
-        //--------------------------
-        std::function<COST_OUTPUT(Args...)> m_CostFunction = nullptr;
-        //--------------------------
+        std::function<COST_OUTPUT(Args...)> m_CostFunction = nullptr; 
     //--------------------------------------------------------------
 };// end class RLEnvironment
 //--------------------------------------------------------------

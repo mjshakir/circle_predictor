6c6c99ce011a9ee4205164cb0690dd61d19c205f
mjshakir <maslove23@gmail.com>
2022-11-02 23:06:21 -0300

    Change: lambda function bind and clean up

--- include/Generate/RLEnvironment.hpp
+++ include/Generate/RLEnvironment.hpp
@@ -8,81 +8,64 @@
 //--------------------------------------------------------------
 #include <torch/torch.h>
 //--------------------------------------------------------------
-// template<typename COST_OUTPUT, typename C, typename... Args>
 template<typename T, typename COST_OUTPUT, typename... Args>
-
+//--------------------------------------------------------------
 class RLEnvironment{
     //--------------------------------------------------------------
     public:
         //--------------------------------------------------------------
         RLEnvironment(void) = delete;
         //--------------------------
-        using CostFunction = std::function<COST_OUTPUT(Args...)>;
-        RLEnvironment(std::vector<T>& data, CostFunction costFunction) :    m_data_iter(data.begin()), 
-                                                                            m_data_iter_end(data.end()), 
-                                                                            m_CostFunction(std::move(costFunction)){
+        RLEnvironment(std::vector<T>&& data, std::function<COST_OUTPUT(Args...)> costFunction) :    m_data(std::move(data)),
+                                                                                                    m_data_iter (m_data.begin()), 
+                                                                                                    m_CostFunction(costFunction){
             //----------------------------
         }// end RLEnvironment(Dataset&& data_loader)
         //--------------------------
-        // template<typename Functions>
-        // void set_reward_function(Functions& function){
-        //     //--------------------------
-        //     m_CostFunction = object_bind(&function, this);
-        //     //--------------------------
-        // }// end void set_reward_function(Functions&& function);
-        //--------------------------
-        // template<typename FUNCTION>
-        // void set_reward_function(COST_OUTPUT (FUNCTION::*fun) (Args...), FUNCTION *t){
-        //     //--------------------------
-        //     m_CostFunction = [t, fun](Args... args){ return (t->*fun) (args...); };
-        //     //--------------------------
-        // }// end void set_reward_function(Functions&& function);
+        std::tuple<torch::Tensor, COST_OUTPUT, bool> step(Args... args){
+            //----------------------------
+            return internal_step(args...);
+            //----------------------------
+        }// std::tuple<torch::Tensor, COST_OUTPUT, bool> step(Args... args)
         //--------------------------------------------------------------
     protected:
         //--------------------------------------------------------------
-        /*template<typename R, typename C, typename... Args>
-        std::function<R(Args...)> objectBind(R (C::* func)(Args...), C& instance) {
-            return [=](Args... args){ return (instance.*func)(args...); };
-        }// end std::function<R(Args...)> objectBind(R (C::* func)(Args...), C& instance) 
-        From: https://stackoverflow.com/questions/14803112/short-way-to-stdbind-member-function-to-object-instance-without-binding-param 
-        and https://stackoverflow.com/questions/70355767/binding-a-class-method-to-a-method-of-another-class */
-        //--------------------------------------------------------------
-        // template<typename C>
-        // std::function<COST_OUTPUT(Args...)> object_bind(COST_OUTPUT (C::* func)(Args...), C& instance){
-        //     return [=](Args... args){ return (instance.*func)(args...); };
-        // }// end std::function<COST_OUTPUT(Args...)> objectBind(R (C::* func)(Args...), C& instance)
-        //--------------------------
-        // std::function<COST_OUTPUT(Args...)> objectBind(COST_OUTPUT (C::* func)(Args...), C& instance) {
-        //     return [=](Args... args){ return (instance.*func)(args...); };
-        // }// end std::function<COST_OUTPUT(Args...)> objectBind(COST_OUTPUT (C::* func)(Args...), C& instance)
-        //--------------------------
         std::tuple<torch::Tensor, COST_OUTPUT, bool> internal_step(Args... args){
-            //--------------------------
-            T _input = *m_data_iter;
             //--------------------------
             auto _reward = m_CostFunction(args...);
             //--------------------------
-            if(m_data_iter == m_data_iter_end){
+            if(m_data_iter == m_data.end()-1){
                 //--------------------------
-                return {torch::tensor(_input), _reward, true};
+                return {*m_data_iter, _reward, true};
                 //--------------------------
-            }// if(m_iter.end())
+            }// if(m_data_iter == m_data.end())
             //--------------------------
-            ++m_data_iter;
+            if (m_data_iter == m_data.begin()){
+                //--------------------------
+                auto input = *m_data_iter;
+                //--------------------------
+                ++m_data_iter;
+                //--------------------------
+                return {*m_data_iter, _reward, false};
+                //--------------------------
+            }// end auto _reward = m_CostFunction(args...)
             //--------------------------
-            return {torch::tensor(_input), _reward, false};
+            if(m_data_iter != m_data.end()){
+                //--------------------------
+                ++m_data_iter;
+                //--------------------------
+            }// if(m_data_iter == m_data.end())
+            //--------------------------
+            return {*m_data_iter, _reward, false};
             //--------------------------
         }// end void internal_step(const ACTION& actions)
         //--------------------------------------------------------------
     private:
         //--------------------------------------------------------------
-        typename std::vector<T>::iterator m_data_iter, m_data_iter_end;
+        std::vector<T> m_data;
+        typename std::vector<T>::iterator m_data_iter;
         //--------------------------
-        // std::function<COST_OUTPUT(Args...)> m_CostFunction = nullptr;
-        //-------------------------- 
-        // using CostFunction = std::function<COST_OUTPUT(Args...)>;
-        // CostFunction m_CostFunction(Args...);
-        CostFunction m_CostFunction;
+        std::function<COST_OUTPUT(Args...)> m_CostFunction;
     //--------------------------------------------------------------
 };// end class RLEnvironment
 //--------------------------------------------------------------

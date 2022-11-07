d4cf6082872970410d72365f28455b7e20f0988b
mjshakir <maslove23@gmail.com>
2022-11-06 22:08:45 -0400

    Change: function name

--- main/rl_main.cpp
+++ main/rl_main.cpp
@@ -18,7 +18,7 @@ int main(void){
     RLGenerate _generate(100, 10);
     //--------------------------
     // std::cout << "generation: " << std::endl;
-    auto input = _generate.get_data();
+    auto input = _generate.get_input();
     // for(const auto& x : input ){
     //     std::cout << x << std::endl;
     // }
@@ -27,18 +27,21 @@ int main(void){
     // // batches into a single tensor.
     // // auto data_set = DataLoader(std::move(std::get<0>(data)), std::move(std::get<1>(data))).map(torch::data::transforms::Normalize<>(0.5, 0.25)).map(torch::data::transforms::Stack<>());
     // //--------------------------
-    // auto data_set = RLDataLoader(_generate.get_data()).map(torch::data::transforms::Stack<>());
+    // auto data_set = RLDataLoader(_generate.get_input()).map(torch::data::transforms::Stack<>());
     // //--------------------------
     // // Generate a data loader.
     // //--------------------------
     // auto data_loader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>( std::move(data_set), 
     //                                                                                         torch::data::DataLoaderOptions(20));
     //--------------------------------------------------------------
-    // RLEnvironment<decltype((_generate.get_data)()), double> _environment(_generate.get_data());
+    // RLEnvironment<decltype((_generate.get_input)()), double> _environment(_generate.get_input());
     RLEnvironment<torch::Tensor, double, double> _environment(std::move(input), [](const double& i){return i + 4.f;});
     //--------------------------
     bool _done = false;
     size_t i = 0;
+    //--------------------------
+    // torch::Tensor tensor = torch::rand({1});
+    // std::cout<< "Tensor " << tensor << std::endl;
     //--------------------------
     while(!_done){
         //--------------------------

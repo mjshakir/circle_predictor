94a605ba88bb480c4d4e660fb5ae4bcea0a76c27
mjshakir <maslove23@gmail.com>
2022-11-07 20:08:47 -0400

    Fix: cost and reference call call that couse error

--- include/Network/ReinforcementNetworkHandling.hpp
+++ include/Network/ReinforcementNetworkHandling.hpp
@@ -18,7 +18,7 @@ class ReinforcementNetworkHandling{
         //--------------------------
         ReinforcementNetworkHandling(   Network& model, 
                                         const torch::Device& device,
-                                        std::function<torch::Tensor(Args&...)> actions):    m_model(model), 
+                                        std::function<torch::Tensor(Args...)> actions):    m_model(model), 
                                                                                             m_device(device),
                                                                                             m_random_action(std::move(actions)){
             //--------------------------
@@ -26,18 +26,20 @@ class ReinforcementNetworkHandling{
         //--------------------------
         ReinforcementNetworkHandling(   Network&& model, 
                                         const torch::Device& device, 
-                                        std::function<torch::Tensor(Args&...)> actions) :   m_model(std::move(model)), 
+                                        std::function<torch::Tensor(Args...)> actions) :   m_model(std::move(model)), 
                                                                                             m_device(device),
                                                                                             m_random_action(std::move(actions)){
             //--------------------------
         }// end ReinforcementNetworkHandling(Network& model, torch::Device& device)
         //--------------------------
-        torch::Tensor action(const torch::Tensor& input, const double& epsilon, const Args... args){
+        torch::Tensor action(torch::Tensor& input, const double& epsilon, Args... args){
+            //--------------------------
             return select_action(input, epsilon, args...);
+            //--------------------------
         }// end torch::Tensor select(const torch::Tensor& input, const double& epsilon, const Args... args)
         //--------------------------
-        void agent( const torch::Tensor& input, 
-                    const torch::Tensor& next_input, 
+        void agent( torch::Tensor& input, 
+                    torch::Tensor& next_input, 
                     torch::optim::Optimizer& optimizer, 
                     const torch::Tensor& rewards, 
                     const bool& done, 
@@ -49,8 +51,7 @@ class ReinforcementNetworkHandling{
     //--------------------------------------------------------------
     protected:
         //--------------------------------------------------------------
-        template<typename T>
-        torch::Tensor select_action(const torch::Tensor& input, const double& epsilon, const Args... args){
+        torch::Tensor select_action(torch::Tensor& input, const double& epsilon, Args... args){
             //--------------------------
             std::random_device rd;  // Will be used to obtain a seed for the random number engine
             std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd(
@@ -64,7 +65,7 @@ class ReinforcementNetworkHandling{
             //--------------------------
             if(_random_egreedy > epsilon){
                 //--------------------------
-                return m_model(input).forward();
+                return m_model.forward(input);
                 //--------------------------
             } // end if(_random_egreedy > epsilon)     
             //--------------------------
@@ -75,14 +76,16 @@ class ReinforcementNetworkHandling{
         // template<typename AGENT_OUTPUT, typename... Args>
         // void agent(std::function<AGENT_OUTPUT(Args&...)> agent_function); 
         //--------------------------
-        void agent_optimizer(   const torch::Tensor& input, 
-                                const torch::Tensor& next_input, 
+        void agent_optimizer(   torch::Tensor& input, 
+                                torch::Tensor& next_input, 
                                 torch::optim::Optimizer& optimizer, 
                                 const torch::Tensor& rewards, 
                                 const bool& done, 
                                 const double& gamma){
             //--------------------------
             torch::Tensor _target_value;
+            //--------------------------
+            m_model.train(true);
             //--------------------------
             if(done){
                 //--------------------------
@@ -91,14 +94,20 @@ class ReinforcementNetworkHandling{
             }// end if(done)
             else{
                 //--------------------------
-                auto _state_value = m_model(next_input).detach();
+                std::cout << "_state_value: "  << std::endl;
+                //--------------------------
+                auto _state_value = m_model.forward(next_input).detach();
                 _target_value = rewards + gamma * _state_value;
+                //--------------------------
+                std::cout << "_target_value: " << _target_value.sizes()  << std::endl;
                 //--------------------------
             }// end else
             //--------------------------
             optimizer.zero_grad();
             //--------------------------
             auto _predicted_value = m_model.forward(input);
+            //--------------------------
+            std::cout << "_predicted_value: " << _predicted_value.sizes()  << std::endl;
             //--------------------------
             torch::Tensor loss = torch::mse_loss(_predicted_value, _target_value);
             //--------------------------
@@ -112,7 +121,7 @@ class ReinforcementNetworkHandling{
         Network m_model; 
         torch::Device m_device;
         //--------------------------
-        std::function<torch::Tensor(Args&...)> m_random_action;
+        std::function<torch::Tensor(Args...)> m_random_action;
     //--------------------------------------------------------------
 };// end class ReinforcementNetworkHandling : protected NetworkHandling
 //--------------------------------------------------------------

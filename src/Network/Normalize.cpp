89f1bd919a9fa678036ca75330c042c750ac2d38
mjshakir <maslove23@gmail.com>
2022-11-10 19:41:25 -0400

    Add: vector tensor normalization

--- src/Network/Normalize.cpp
+++ src/Network/Normalize.cpp
@@ -3,9 +3,57 @@
 //--------------------------------------------------------------
 #include "Network/Normalize.hpp"
 //--------------------------------------------------------------
-
+// Standard cpp library
+//--------------------------------------------------------------
+#include <algorithm>
+#include <execution>
 //--------------------------------------------------------------
 Normalize::Normalize(const torch::Tensor& input) : m_input(input), m_max(torch::max(input)), m_min(torch::min(input)){
+    //--------------------------
+}// end Normalize::Normalize(const torch::Tensor& input)
+//--------------------------------------------------------------
+Normalize::Normalize(const std::vector<torch::Tensor>& input) : m_vinput(input){
+    //--------------------------
+    torch::Tensor min = torch::tensor(10000), max = torch::tensor(0);
+    //--------------------------
+    // std::for_each(std::execution::par_unseq, input.begin(), input.end(), [&](const auto& x){
+    //     //--------------------------
+    //     torch::Tensor temp_min = torch::min(x), temp_max = torch::max(x);
+    //     //--------------------------
+    //     if(temp_min.less(min).any().item<bool>()){
+    //         //--------------------------
+    //         min = temp_min;
+    //         //--------------------------
+    //     }// end if(temp_min.less(min).any().item<bool>())
+    //     if(temp_max.greater(max).any().item<bool>()){
+    //         //--------------------------
+    //         max = temp_max;
+    //         //--------------------------
+    //     }// end if(max.less(temp_max).any().item<bool>())
+    // });
+    //--------------------------
+    for(const auto& x : input){  
+        //--------------------------
+        torch::Tensor temp_min = torch::min(x), temp_max = torch::max(x);
+        //--------------------------
+        if(temp_min.less(min).any().item<bool>()){
+            //--------------------------
+            min = temp_min;
+            //--------------------------
+        }// end if(temp_min.less(min).any().item<bool>())
+        //--------------------------
+        if(temp_max.greater(max).any().item<bool>()){
+            //--------------------------
+            max = temp_max;
+            //--------------------------
+        }// end if(max.less(temp_max).any().item<bool>())
+        //--------------------------
+    }; // end for(const auto& x : input)
+    //--------------------------
+    m_min = min;
+    m_max = max;
+    //--------------------------
+    // std::cout << "min: " << min << " max: " << max << std::endl;
     //--------------------------
 }// end Normalize::Normalize(const torch::Tensor& input)
 //--------------------------------------------------------------
@@ -15,7 +63,25 @@ torch::Tensor Normalize::normalization(void){
     //--------------------------
 }// end torch::Tensor Normalize::normalization(void)
 //--------------------------------------------------------------
+std::vector<torch::Tensor> Normalize::vnormalization(void){
+    //--------------------------
+    return normalization_vdata();
+    //--------------------------
+}// end torch::Tensor Normalize::normalization(void)
+//--------------------------------------------------------------
+torch::Tensor Normalize::vnormalization(const torch::Tensor& input){
+    //--------------------------
+    return normalization_vdata(input);
+    //--------------------------
+}// end torch::Tensor Normalize::normalization(void)
+//--------------------------------------------------------------
 torch::Tensor Normalize::normalization(const torch::Tensor& input){
+    //--------------------------
+    return normalization_data(input);
+    //--------------------------
+}// end torch::Tensor Normalize::normalization(const torch::Tensor& input)
+//--------------------------------------------------------------
+std::vector<torch::Tensor> Normalize::normalization(const std::vector<torch::Tensor>& input){
     //--------------------------
     return normalization_data(input);
     //--------------------------
@@ -33,9 +99,82 @@ torch::Tensor Normalize::normalization_data(void){
     //--------------------------
 }// end torch::Tensor Normalize::normalization_data(void)
 //--------------------------------------------------------------
+std::vector<torch::Tensor> Normalize::normalization_vdata(void){
+    //--------------------------
+    std::vector<torch::Tensor> _data;
+    _data.reserve(m_vinput.size());
+    //--------------------------
+    for(const auto& x : m_vinput){
+        //--------------------------
+        _data.push_back(((x-m_min)/(m_max-m_min)));
+        //--------------------------
+    }// for(const auto& x : input)
+    //--------------------------
+    return _data;
+    //--------------------------
+}// end torch::Tensor Normalize::normalization_data(void)
+//--------------------------------------------------------------
+torch::Tensor Normalize::normalization_vdata(const torch::Tensor& input){
+    //--------------------------
+    return ((input-m_min)/(m_max-m_min));
+    //--------------------------
+}// end torch::Tensor Normalize::normalization_data(void)
+//--------------------------------------------------------------
 torch::Tensor Normalize::normalization_data(const torch::Tensor& input){
     //--------------------------
     return ((input-torch::min(input))/(torch::max(input)-torch::min(input)));
+    //--------------------------
+}// end torch::Tensor Normalize::normalization_data(const torch::Tensor& input)
+//--------------------------------------------------------------
+std::vector<torch::Tensor> Normalize::normalization_data(const std::vector<torch::Tensor>& input){
+    //--------------------------
+    torch::Tensor min = torch::tensor(10000), max = torch::tensor(0);
+    std::vector<torch::Tensor> _data;
+    _data.reserve(input.size());
+    //--------------------------
+    for(const auto& x : input){  
+        //--------------------------
+        torch::Tensor temp_min = torch::min(x), temp_max = torch::max(x);
+        //--------------------------
+        if(temp_min.less(min).any().item<bool>()){
+            //--------------------------
+            min = temp_min;
+            //--------------------------
+        }// end if(temp_min.less(min).any().item<bool>())
+        //--------------------------
+        if(temp_max.greater(max).any().item<bool>()){
+            //--------------------------
+            max = temp_max;
+            //--------------------------
+        }// end if(max.less(temp_max).any().item<bool>())
+        //--------------------------
+    }; // end for(const auto& x : input)
+    //--------------------------
+    // std::for_each(std::execution::par_unseq, input.begin(), input.end(), [&](const auto& x){
+    //     //--------------------------
+    //     torch::Tensor temp_min = torch::min(x), temp_max = torch::max(x);
+    //     //--------------------------
+    //     if(temp_min.less(min).any().item<bool>()){
+    //         //--------------------------
+    //         min = temp_min;
+    //         //--------------------------
+    //     }// end if(temp_min.less(min).any().item<bool>())
+    //     if(max.less(temp_max).any().item<bool>()){
+    //         //--------------------------
+    //         max = temp_max;
+    //         //--------------------------
+    //     }// end if(max.less(temp_max).any().item<bool>())
+    // });
+    //--------------------------
+    // std::cout << "it_max: " << max << " it_min: " << min << std::endl;
+    //--------------------------
+    for(const auto& x : input){
+        //--------------------------
+        _data.push_back(((x-min)/(max-min)));
+        //--------------------------
+    }// for(const auto& x : input)
+    //--------------------------
+    return _data;
     //--------------------------
 }// end torch::Tensor Normalize::normalization_data(const torch::Tensor& input)
 //--------------------------------------------------------------

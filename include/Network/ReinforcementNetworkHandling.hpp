f5f1d586c20c6ecaf1d6192a276623c15c507d6a
mjshakir <maslove23@gmail.com>
2023-01-19 22:08:47 -0400

    Add: nan protection, a test methon for optimize

--- include/Network/ReinforcementNetworkHandling.hpp
+++ include/Network/ReinforcementNetworkHandling.hpp
@@ -8,6 +8,7 @@
 // Standard library
 //--------------------------------------------------------------
 #include <random>
+#include <math.h>
 //--------------------------------------------------------------
 template<typename Network, typename... Args>
 class ReinforcementNetworkHandling{
@@ -46,6 +47,16 @@ class ReinforcementNetworkHandling{
                     const double& gamma = 0.5){
             //--------------------------
             agent_optimizer(input, next_input, optimizer, rewards, done, gamma);
+            //--------------------------
+        }// end void agent
+        //--------------------------
+        void agent( const torch::Tensor& input, 
+                    torch::optim::Optimizer& optimizer, 
+                    const torch::Tensor& rewards, 
+                    const bool& done, 
+                    const double& gamma = 0.5){
+            //--------------------------
+            agent_optimizer(input, optimizer, rewards, done, gamma);
             //--------------------------
         }// end void agent
     //--------------------------------------------------------------
@@ -105,6 +116,61 @@ class ReinforcementNetworkHandling{
                 // std::cout << "_target_value: " << _target_value.sizes()  << std::endl;
                 //--------------------------
             }// end else
+            //--------------------------
+            optimizer.zero_grad();
+            //--------------------------
+            // std::cout << " _predicted_value input: " << input.sizes()  << std::endl;
+            //--------------------------
+            auto _predicted_value = m_model.forward(_input);
+            //--------------------------
+            // std::cout << "_predicted_value: " << _predicted_value.sizes()  << std::endl;
+            //--------------------------
+            // std::cout << "_predicted_value: " << _predicted_value  << "\n _target_value: " << _target_value << std::endl;
+            //--------------------------
+            torch::Tensor loss = torch::mse_loss(_predicted_value, _target_value);
+            //--------------------------
+            if(torch::isnan(loss).any().item<bool>()){
+                //--------------------------
+                throw std::overflow_error("\x1b[31m" "\033[1m" "NaN is detected" "\033[m" "\x1b[0m");
+                //--------------------------
+            }//end if( torch::isnan(_predicted_value) || torch::isnan(_target_value))
+            //--------------------------
+            // std::cout << "loss "  << std::endl;
+            //--------------------------
+            loss.backward({},c10::optional<bool>(true), false);
+            optimizer.step();
+            //-------------------------- 
+        }// end void agent(const torch::Tensor& input, const torch::Tensor& next_input, const torch::Tensor& action, const T& rewards, const bool& done)
+        //--------------------------
+        void agent_optimizer(   const torch::Tensor& input, 
+                                torch::optim::Optimizer& optimizer, 
+                                const torch::Tensor& rewards, 
+                                const bool& done, 
+                                const double& gamma){
+            //--------------------------
+            torch::Tensor _input = input;
+            //--------------------------
+            std::random_device rd;  // Will be used to obtain a seed for the random number engine
+            std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd(
+            std::uniform_real_distribution<double> uniform_angle(0., 2*M_PI);
+            std::default_random_engine re;
+            //--------------------------
+            std::vector<double> _points;
+            _points.reserve(2);
+            //--------------------------
+            auto _angle = uniform_angle(re);
+            _points.push_back(((_input[-1][0] * torch::tensor(std::sin(_angle)))+_input[-1][1]).item<double>());
+            _points.push_back(((_input[-1][0] * torch::tensor(std::cos(_angle)))+_input[-1][2]).item<double>());
+            //--------------------------
+            auto _target_value = torch::tensor(_points).view({-1,2});
+            //--------------------------
+            if(!done){
+                //--------------------------
+                _target_value = rewards + gamma * _target_value;
+                //--------------------------
+            }// end if(done)
+            //--------------------------
+            m_model.train(true);
             //--------------------------
             optimizer.zero_grad();
             //--------------------------

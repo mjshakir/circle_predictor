314ab86bb15f21ebea8e920e0824d75a43b4904f
mjshakir <maslove23@gmail.com>
2023-01-28 03:43:11 -0400

    Change: name

--- include/Network/Network.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Network/Networks.hpp"
-#include "Network/NetworkHandling.hpp"
-#include "Network/DataLoader.hpp"
-#include "Network/Normalize.hpp"
-#include "ReinforcementNetworkHandling.hpp"

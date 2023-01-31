2fc2f195c03e0887d348fc3f83bbab5f49e4f276
mjshakir <maslove23@gmail.com>
2023-01-31 06:04:03 -0400

    Fix: debault radomizer and range bug

--- src/Network/RL/ExperienceReplay.cpp
+++ src/Network/RL/ExperienceReplay.cpp
@@ -40,19 +40,43 @@ void ExperienceReplay::push_data(const torch::Tensor& input, const torch::Tensor
         //--------------------------
         m_position = ++m_position % m_capacity;
         //--------------------------
+        // if(m_position == 0){
+        //     //--------------------------
+        //     m_memory.clear();
+        //     //--------------------------
+        // }// end if(m_position == 0)
+        //--------------------------
     }// end if(m_position != LONG_MAX)
     //--------------------------
 }// end void ExperienceReplay::push_data(const torch::Tensor& input, const torch::Tensor& next_input, const torch::Tensor& reward, const bool& done)
 //--------------------------------------------------------------
 std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, bool> ExperienceReplay::sample_data(void){
     //--------------------------
-    std::random_device rd;  // Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd(
-    std::uniform_int_distribution<size_t> uniform_position(0, m_memory.size());
-    std::default_random_engine re;
+    // std::random_device rd;  // Will be used to obtain a seed for the random number engine
+    // std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd(
+    // std::uniform_int_distribution<std::mt19937::result_type> uniform_position(0, m_memory.size());
+    // std::default_random_engine re;
     //--------------------------
-    auto it = m_memory.begin();
-    std::advance(it, uniform_position(re));
+    // size_t random_ = uniform_position(re);
+    //--------------------------
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> uniform_position(0, m_memory.size()-1);
+    //--------------------------
+    size_t random_ = uniform_position(rng);
+    //--------------------------
+    // std::cout   << "memory size: [" << m_memory.size() 
+    //             << "] random_: [" << random_ << "] ";
+    //--------------------------
+    // auto it = m_memory.begin();
+    // std::advance(it, random_);
+    // //--------------------------
+    auto it = std::next( m_memory.begin() , random_);
+    //--------------------------
+    // auto it = std::ranges::next( m_memory.begin() , random_, m_memory.end());
+    //--------------------------
+    // std::cout   << "memory picked: [" << it->first 
+    //             << "] positions: [" << m_position << "]" << std::endl;
     //--------------------------
     return it->second;
     //--------------------------

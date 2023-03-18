8ce0b11d391a94873e8e4e198b090d656c955bac
mjshakir <maslove23@gmail.com>
2023-03-18 08:17:02 -0300

    Change: comments

--- include/Generate/RL/RLGenerate.hpp
+++ include/Generate/RL/RLGenerate.hpp
@@ -9,54 +9,60 @@ class RLGenerate{
     //--------------------------------------------------------------
     public:
         //--------------------------------------------------------------
-        RLGenerate() = delete;
+        RLGenerate(void) = delete;
         //--------------------------
         /**
-         *  @brief A constructor. Generate both X and Y points. 
-         *
-         *  @tparam radius: circle raduis default value = 1.
-         *  @tparam generated_points: How many points to generate
-         *  @tparam center: points of the circle. 
+         *  @brief A constructor. Generate both X, Y center point of the circle, and the radius. 
+         *  it also generate test data that 20% of the generated_points
+         *  @param generated_points: How many points to generate. @default: 60000
+         *  @param column: helps design the structure of the output. @default: 2
+         *  @param limiter: the maximum value the points can get for X, Y. @default: 10.0
          */
-        RLGenerate(const size_t& generated_points = 60000, const size_t& column = 2, const double& limiter = 10.f);
+        RLGenerate(const size_t& generated_points = 60000, const size_t& column = 2, const double& limiter = 10.);
         //--------------------------
         /**
-         *  @brief A constructor. Generate both X and Y points. 
-         *
-         *  @tparam radius: circle raduis default value = 1.
-         *  @tparam generated_points: How many points to generate
-         *  @tparam center: points of the circle. 
+         *  @brief A constructor. Generate both X, Y center point of the circle, and the radius. 
+         *  it also generate test data that 20% of the generated_points
+         *  @param generated_points: How many points to generate. @default: 60000
+         *  @param generated_points_test: How many points to test data is generated. @default: 10000
+         *  @param column: helps design the structure of the output. @default: 2
+         *  @param limiter: the maximum value the points can get for X, Y. @default: 10.0
          */
         RLGenerate( const size_t& generated_points = 60000,
                     const size_t& generated_points_test = 10000, 
                     const size_t& column = 2, 
-                    const double& limiter = 10.f);
+                    const double& limiter = 10.);
         //--------------------------
         /**
-         *  @brief Getter: the network input and target data set.  
-         *
-         *  @return 1) x_value: input data. 2)y_value: target data. 
+         *  @brief Getter: The data generated.  
+         *  
+         *  @return td::vector<torch::Tensor>: Return the data X,Y, Radius.
          */
         std::vector<torch::Tensor> get_input(void);
         //--------------------------
         /**
-         *  @brief Getter: the network input and target data set.  
-         *
-         *  @return 1) x_value: input data. 2)y_value: target data. 
+         *  @brief Getter: The test data generated.  
+         *  
+         *  @return std::vector<torch::Tensor>: Return the data X,Y, Radius.
          */
         std::vector<torch::Tensor> get_test_input(void);
         //--------------------------
         /**
-         *  @brief Getter: the network input and target data set.  
-         *
-         *  @return 1) x_value: input data. 2)y_value: target data. 
+         *  @brief Getter: the target data set.
+         *   
+         *  @param generated_points: How many points to generate. @default: 60000
+         *  @param column: helps design the structure of the output. @default: 2
+         * 
+         *  @return torch::Tensor: target data.
          */
         torch::Tensor get_output(const size_t& generated_points = 60000, const size_t& column = 2);
         //--------------------------
         /**
-         *  @brief Getter: the network input and target data set.  
-         *
-         *  @return 1) x_value: input data. 2)y_value: target data. 
+         * @brief Generated data.
+         * 
+         * @param generated_points: How many points to generate. @default: 60000
+         * @param column: helps design the structure of the output. @default: 2
+         * @return std::vector<torch::Tensor>: the data
          */
         std::vector<torch::Tensor> data(const size_t& generated_points = 60000, const size_t& column = 3);
         //--------------------------------------------------------------
@@ -72,10 +78,7 @@ class RLGenerate{
         //--------------------------------------------------------------
     private:
         //--------------------------------------------------------------
-        size_t m_generated_points, m_column;
         double m_limiter;
-        //--------------------------
-        std::binary_semaphore m_data_sem{0}, m_data_test_sem{0};
         //--------------------------
         std::vector<torch::Tensor> m_data, m_data_test;
     //--------------------------------------------------------------

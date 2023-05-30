a553c278e787fae67f1aee1d4e2e941d608d09dc
mjshakir <maslove23@gmail.com>
2023-05-30 18:54:18 -0300

    Change: refactoring

--- src/Generate/RL/Generate.cpp
+++ src/Generate/RL/Generate.cpp
@@ -3,32 +3,28 @@
 //--------------------------------------------------------------
 #include "Generate/RL/Generate.hpp"
 //--------------------------------------------------------------
-namespace RL {
-    //--------------------------------------------------------------
-    //short form to get the previous even: x &= ~1;
-    Generate::Generate( const size_t& generated_points) :   m_generated_points((generated_points%2 == 0) ? generated_points : generated_points - (generated_points & 1)), 
+//short form to get the previous even: x &= ~1;
+RL::Generate::Generate( const size_t& generated_points) :   m_generated_points((generated_points%2 == 0) ? generated_points : generated_points - (generated_points & 1)), 
                                                             m_generated_points_test(m_generated_points*0.2){
-        //--------------------------
-    }// end Generate::Generate(const size_t& generated_points, const size_t& column, const double& limiter)
-    //--------------------------------------------------------------
-    RL::Generate::Generate( const size_t& generated_points, 
-                        const size_t& generated_points_test) : m_generated_points((generated_points%2 == 0) ? generated_points : generated_points - (generated_points & 1)),
-                                                               m_generated_points_test((generated_points_test%2 == 0 and generated_points_test==0) ? 
-                                                                    generated_points_test : generated_points_test - (generated_points_test & 1)){
-        //--------------------------
-    }// end Generate::Generate( const size_t& generated_points, const size_t& generated_points_test, const size_t& column, const double& limiter)
-    //--------------------------------------------------------------
-    const size_t& Generate::get_generated_points(void) const{
-        //--------------------------
-        return m_generated_points;
-        //--------------------------
-    }// end const size_t& Generate::get_generated_points(void)
-    //--------------------------------------------------------------
-    const size_t& Generate::get_generated_points_test(void) const{
-        //--------------------------
-        return m_generated_points_test;
-        //--------------------------
-    }// end const size_t& Generate::get_generated_points(void)
-    //--------------------------------------------------------------
-}// end namespace RL
+    //--------------------------
+}// end RL::Generate::Generate(const size_t& generated_points, const size_t& column, const double& limiter)
+//--------------------------------------------------------------
+RL::Generate::Generate( const size_t& generated_points, 
+                    const size_t& generated_points_test) : m_generated_points((generated_points%2 == 0) ? generated_points : generated_points - (generated_points & 1)),
+                                                           m_generated_points_test((generated_points_test%2 == 0 and generated_points_test==0) ? 
+                                                                generated_points_test : generated_points_test - (generated_points_test & 1)){
+    //--------------------------
+}// end RL::Generate::Generate( const size_t& generated_points, const size_t& generated_points_test, const size_t& column, const double& limiter)
+//--------------------------------------------------------------
+const size_t& RL::Generate::get_generated_points(void) const{
+    //--------------------------
+    return m_generated_points;
+    //--------------------------
+}// end const size_t& RL::Generate::get_generated_points(void)
+//--------------------------------------------------------------
+const size_t& RL::Generate::get_generated_points_test(void) const{
+    //--------------------------
+    return m_generated_points_test;
+    //--------------------------
+}// end const size_t& RL::Generate::get_generated_points(void)
 //--------------------------------------------------------------

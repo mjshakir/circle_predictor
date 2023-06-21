186f3d5ab42b8f63db2de217d50639946b1ff588
mjshakir <maslove23@gmail.com>
2023-06-21 07:34:54 -0300

    Remove: extra functions

--- include/Generate/Supervised/Normalize.hpp
+++ include/Generate/Supervised/Normalize.hpp
@@ -20,25 +20,11 @@ class Normalize{
         Normalize(const torch::Tensor& input);
         //--------------------------
         /**
-         *  @brief A constructor 
-         *
-         *  @tparam input: a torch tensor
-         */
-        Normalize(const std::vector<torch::Tensor>& input);
-        //--------------------------
-        /**
          *  @brief Normalize the input tensor from the constructor. This uses Min-max feature scaling.
          *
          *  @return A normalized a torch tensor
          */
         torch::Tensor normalization(void);
-        //--------------------------
-        /**
-         *  @brief Normalize the input tensor from the constructor. This uses Min-max feature scaling.
-         *
-         *  @return A normalized a torch tensor
-         */
-        std::vector<torch::Tensor> vnormalization(void);
         //--------------------------
         /**
          *  @brief Normalize the input tensor from the constructor. This uses Min-max feature scaling.
@@ -56,16 +42,6 @@ class Normalize{
          *  @return A normalized a torch tensor
          */
         static torch::Tensor normalization(const torch::Tensor& input);
-        //--------------------------
-        /**
-         *  @brief  A static Normalizing function. This uses Min-max feature scaling. 
-         *          Warning: cannot use unnormalization.
-         * 
-         *  @warning Cannot use unnormalization since you are using a static function. 
-         *
-         *  @return A normalized a torch tensor
-         */
-        static std::vector<torch::Tensor> normalization(const std::vector<torch::Tensor>& input);
         //--------------------------
         /**
          *  @brief  Unnormalize the input tensor. This uses inverse Min-max feature scaling.
@@ -90,13 +66,9 @@ class Normalize{
         //--------------------------------------------------------------
         torch::Tensor normalization_data(void);
         //--------------------------
-        std::vector<torch::Tensor> normalization_vdata(void);
-        //--------------------------
         torch::Tensor normalization_vdata(const torch::Tensor& input);
         //--------------------------
         static torch::Tensor normalization_data(const torch::Tensor& input);
-        //--------------------------
-        static std::vector<torch::Tensor> normalization_data(const std::vector<torch::Tensor>& input);
         //--------------------------
         torch::Tensor unnormalization_data(const torch::Tensor& input);
         //--------------------------
@@ -110,7 +82,5 @@ class Normalize{
     private:
         //--------------------------------------------------------------
         torch::Tensor m_input, m_max, m_min;
-        //--------------------------
-        std::vector<torch::Tensor> m_vinput;
     //--------------------------------------------------------------
 };

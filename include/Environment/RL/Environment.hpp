9d90d20816cc9f349e003dcf46b8a9edad1c1d33
mjshakir <maslove23@gmail.com>
2023-07-15 13:44:19 -0300

    Add: new classes

--- include/Environment/RL/Environment.hpp
+++ include/Environment/RL/Environment.hpp
@@ -5,6 +5,8 @@
 #include "Environment/RL/EnvironmentTestLoader.hpp"
 #include "Environment/RL/RLEnvironment.hpp"
 #include "Environment/RL/RLEnvironmentLoader.hpp"
+#include "Environment/RL/RLEnvironmentShuffle.hpp"
+#include "Environment/RL/RLEnvironmentShuffleLoader.hpp"
+#include "Environment/RL/RLEnvironmentAtomic.hpp"
 #include "Environment/RL/RLEnvironmentLoaderAtomic.hpp"
-#include "Environment/RL/RLEnvironmentRandomLoader.hpp"
 //--------------------------------------------------------------

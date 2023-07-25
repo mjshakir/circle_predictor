1c232f7f40448eb36cd69c6327fceda9f8b14b27
mjshakir <maslove23@gmail.com>
2023-07-25 17:33:48 -0300

    Add: environment class generators

--- include/Environment/RL/Environment.hpp
+++ include/Environment/RL/Environment.hpp
@@ -12,3 +12,48 @@
 #include "Environment/RL/RLEnvironmentShuffleAtomic.hpp"
 #include "Environment/RL/RLEnvironmentShuffleAtomicLoader.hpp"
 //--------------------------------------------------------------
+namespace RL {
+    //--------------------------------------------------------------
+    namespace Environment {
+        //--------------------------------------------------------------
+        enum TYPE{
+            //----------------------------
+            NORMAL = 0,
+            ATOMIC,
+            SHUFFLE,
+            ATOMIC_SHUFFLE
+            //----------------------------
+        }//end enum TYPE
+        //--------------------------------------------------------------
+        class Environment {
+            //--------------------------------------------------------------
+            public:
+                //--------------------------------------------------------------
+            protected:
+                //--------------------------------------------------------------
+                template<typename T, typename COST_FUNCTION, typename... Args>
+                static auto generate_environment(   std::vector<T>&& data, 
+                                                    COST_FUNCTION&& costFunction,
+                                                    const double& egreedy = 0.9,
+                                                    const double& egreedy_final = 0.02,
+                                                    const double& egreedy_decay = 500.,
+                                                    const TYPE& type = TYPE::NORMAL);
+                //----------------------------
+                template<typename T, typename COST_FUNCTION, typename... Args>
+                static auto generate_environment(   std::vector<T>&& data, 
+                                                    COST_FUNCTION&& costFunction,
+                                                    const size_t batch = 2,
+                                                    const double& egreedy = 0.9,
+                                                    const double& egreedy_final = 0.02,
+                                                    const double& egreedy_decay = 500.,
+                                                    const TYPE& type= TYPE::NORMAL);
+                //----------------------------
+                template<typename T>
+                static auto generate_environment(std::vector<T>&& data, const size_t batch = 2);
+                //--------------------------------------------------------------
+        };// end class RLEnvironment
+        //--------------------------------------------------------------
+    }// end namespace Environment
+    //--------------------------------------------------------------
+}// end namespace RL
+//--------------------------------------------------------------
--- src/Environment/RL/Environment.cpp
+++ src/Environment/RL/Environment.cpp
@@ -3,3 +3,124 @@
 //--------------------------------------------------------------
 #include "Environment/RL/Environment.hpp"
 //--------------------------------------------------------------
+// Standard cpp library
+//--------------------------------------------------------------
+#include <memory>
+//--------------------------------------------------------------
+template<typename T, typename COST_FUNCTION, typename... Args>
+auto RL::Environment::Environment::generate_environment( std::vector<T>&& data, 
+                                        COST_FUNCTION&& costFunction,
+                                        const double& egreedy,
+                                        const double& egreedy_final,
+                                        const double& egreedy_decay,
+                                        const TYPE& type){
+    //----------------------------
+    switch(type){
+        //----------------------------
+        case NORMAL:
+            return std::make_unique<RL::Environment::RLEnvironment>( std::move(data),
+                                                    std::move(costFunction),
+                                                    egreedy,
+                                                    egreedy_final,
+                                                    egreedy_decay);
+        //----------------------------
+        case ATOMIC:
+            return std::make_unique<RL::Environment::RLEnvironmentAtomic>(   std::move(data),
+                                                            std::move(costFunction),
+                                                            egreedy,
+                                                            egreedy_final,
+                                                            egreedy_decay);
+        //----------------------------
+        case SHUFFLE:
+            return std::make_unique<RL::Environment::RLEnvironmentShuffle>(  std::move(data),
+                                                            std::move(costFunction),
+                                                            egreedy,
+                                                            egreedy_final,
+                                                            egreedy_decay);
+        //----------------------------
+        case ATOMIC_SHUFFLE:
+            return std::make_unique<RL::Environment::RLEnvironmentShuffleAtomic>(std::move(data),
+                                                                std::move(costFunction),
+                                                                egreedy,
+                                                                egreedy_final,
+                                                                egreedy_decay);
+        //----------------------------
+        default:
+            //----------------------------
+            std::invalid_argument("TYPE be between" std:to_string(TYPE::NORMAL) " and " std:to_string(TYPE::ATOMIC_SHUFFLE));
+            //----------------------------
+    }//end switch(type)
+    //----------------------------
+    return std::make_unique<RL::Environment::RLEnvironment>( std::move(data),
+                                            std::move(costFunctionß),
+                                            egreedy,
+                                            egreedy_final,
+                                            egreedy_decay);
+    //----------------------------
+}//end auto RL::Environment::Environment::generate_environment
+//--------------------------------------------------------------
+auto RL::Environment:: Environment::generate_environment( std::vector<T>&& data, 
+                                        COST_FUNCTION&& costFunction,
+                                        const size_t batch,
+                                        const double& egreedy,
+                                        const double& egreedy_final,
+                                        const double& egreedy_decay,
+                                        const TYPE& type){
+    //----------------------------
+    switch(type){
+        //----------------------------
+        case NORMAL:
+            return std::make_unique<RL::Environment::RLEnvironmentLoader>(   std::move(data),
+                                                            std::move(costFunction),
+                                                            batch,
+                                                            egreedy,
+                                                            egreedy_final,
+                                                            egreedy_decay);
+        //----------------------------
+        case ATOMIC:
+            return std::make_unique<RL::Environment::RLEnvironmentLoaderAtomic>( std::move(data),
+                                                                std::move(costFunction),
+                                                                batch,
+                                                                egreedy,
+                                                                egreedy_final,
+                                                                egreedy_decay);
+        //----------------------------
+        case SHUFFLE:
+            return std::make_unique<RL::Environment::RLEnvironmentShuffleLoader>(std::move(data),
+                                                                std::move(costFunction),
+                                                                batch,
+                                                                egreedy,
+                                                                egreedy_final,
+                                                                egreedy_decay);
+        //----------------------------
+        case ATOMIC_SHUFFLE:
+            return std::make_unique<RL::Environment::RLEnvironmentShuffleAtomicLoader>(  std::move(data),
+                                                                        std::move(costFunction),
+                                                                        batch,
+                                                                        egreedy,
+                                                                        egreedy_final,
+                                                                        egreedy_decay);
+        //----------------------------
+        default:
+            //----------------------------
+            std::invalid_argument("TYPE be between" std:to_string(TYPE::NORMAL) " and " std:to_string(TYPE::ATOMIC_SHUFFLE));
+            //----------------------------
+    }//end switch(type)
+    //----------------------------
+    template<typename T, typename COST_FUNCTION, typename... Args>
+    return std::make_unique<RL::Environment::RLEnvironmentLoader>(   std::move(data),
+                                                    std::move(costFunction),
+                                                    batch,
+                                                    egreedy,
+                                                    egreedy_final,
+                                                    egreedy_decay);
+    //----------------------------        
+}//end auto RL::Environment::Environment::generate_environment
+//--------------------------------------------------------------
+template<typename T>
+auto RL::Environment::Environment::generate_environment(std::vector<T>&& data, const size_t batch){
+    //----------------------------
+    return std::make_unique<RL::Environment::EnvironmentTestLoader<T>> _environment_test(std::move(data), batch);
+    //----------------------------
+}// end auto RL::Environment::Environment::generate_environment(std::vector<T>&& data, const size_t batch)
+//--------------------------------------------------------------

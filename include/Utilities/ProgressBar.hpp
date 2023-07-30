953b20697f0bf4ddc96886e13d29e7ec154aa8f4
mjshakir <maslove23@gmail.com>
2023-07-30 00:22:25 -0300

    Change: bar size to globe, remove: mutex

--- include/Utilities/ProgressBar.hpp
+++ include/Utilities/ProgressBar.hpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <chrono>
 #include <string>
-#include <mutex>
 //--------------------------------------------------------------
 namespace Utils{
     //--------------------------------------------------------------
@@ -14,14 +13,23 @@ namespace Utils{
         //--------------------------------------------------------------
         public:
             //--------------------------------------------------------------
-            ProgressBar(const std::string& name = "Progress:", 
-                        const std::string& progress_char = "+", 
-                        const std::string& empty_space_char = "-");
-            //--------------------------
-            ProgressBar(const size_t& total, 
+            ProgressBar(const uint8_t& bar_length = 30U,
                         const std::string& name = "Progress:", 
                         const std::string& progress_char = "+", 
                         const std::string& empty_space_char = "-");
+            //--------------------------
+            ProgressBar(const size_t& total,
+                        const uint8_t& bar_length = 30U,
+                        const std::string& name = "Progress:", 
+                        const std::string& progress_char = "+", 
+                        const std::string& empty_space_char = "-");
+            //--------------------------
+            ProgressBar           (ProgressBar const&) = delete;
+            ProgressBar& operator=(ProgressBar const&) = delete;
+            ProgressBar           (ProgressBar&&)      = delete;
+            ProgressBar& operator=(ProgressBar&&)      = delete;
+            //--------------------------
+            ~ProgressBar(void)                         = default;
             //--------------------------
             void update(void);
             //--------------------------
@@ -29,11 +37,15 @@ namespace Utils{
             //--------------------------------------------------------------
         protected:
             //--------------------------------------------------------------
+            void append_elapsed_time(std::stringstream& ss);
+            //--------------------------
+            void append_remaining_time(std::stringstream& ss);
+            //--------------------------
             void display(void);
             //--------------------------
             void tick(void);
             //--------------------------
-            constexpr bool is_done(void);
+            inline bool is_done(void) const ;
             //--------------------------
             double get_remaining_seconds(void);
             //--------------------------
@@ -45,6 +57,8 @@ namespace Utils{
             //--------------------------------------------------------------
             size_t m_total, m_progress;
             //--------------------------
+            const uint8_t m_bar_length;
+            //--------------------------
             const std::string m_name, m_progress_char, m_empty_space_char;
             //--------------------------
             bool m_is_first_tick;
@@ -52,8 +66,6 @@ namespace Utils{
             std::chrono::steady_clock::time_point m_last_tick_time, m_start_time;
             //--------------------------
             std::chrono::duration<double> m_average_delta_time;
-            //--------------------------
-            std::mutex m_mutex;
         //--------------------------------------------------------------
     };// end class ProgressBar
     //--------------------------------------------------------------
